c829a14c8efe88ff6016733e9c17f0599a7f5f38
pedroelton <pedroelton@msn.com>
2023-12-20 21:41:00 +0900

    Initial commit for AppleFrameworks_SA_SwiftUI

---
+++ Apple_Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple_Frameworks
+//
+//  Created by Pedro Oliveira on 2023/12/20.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
